d462c162a7bb2437b65131fe988875d46548ffe9
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-21 21:42:06 +0200

    Create a DbContext along with the ImageProjectDbContext

---
+++ Models/ImageProjectDBContext/MyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageProject.Models.ImageProjectDBContext
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<AlbumCategory> AlbumCategories { get; set; }
+        public DbSet<MyMedia> MyMedia { get; set; }
+    }
+}
