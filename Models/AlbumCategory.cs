1936a1ab63d9f487b23c41097854d8c04caf7188
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-21 21:23:42 +0200

    Create AlbumCategory

---
+++ Models/AlbumCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageProject.Models
+{
+    public class AlbumCategory
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public List<MyMedia> MyMedia { get; set; } = new List<MyMedia>();
+    }
+}
