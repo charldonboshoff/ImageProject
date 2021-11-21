8c7d18c16ca967e8afd1850586362e2b73f2d859
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-21 21:27:35 +0200

    Create MyMedia with the reference to AlbumCategory

---
+++ Models/MyMedia.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageProject.Models
+{
+    public class MyMedia
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+        public int AlbumCategoryId { get; set; }
+        public AlbumCategory AlbumCategory { get; set; } = new AlbumCategory();
+    }
+}
