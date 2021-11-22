cae3417643c65b3c04b69e6b1a763e0c72f761a6
charldonboshoff <30074800@student.g.nwu.ac.za>
2021-11-22 14:07:58 +0200

    Create views for create, delete, edit, details and index

--- Data/MyContext.cs
+++ Data/MyContext.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ImageProject.ViewModels.AlbumCategoryViewModels;
+using ImageProject.ViewModels.MyMediaViewModels;
 
 namespace ImageProject.Data
 {
@@ -15,5 +17,9 @@ namespace ImageProject.Data
         }
         public DbSet<AlbumCategory> AlbumCategories { get; set; }
         public DbSet<MyMedia> MyMedia { get; set; }
+        public DbSet<ImageProject.ViewModels.AlbumCategoryViewModels.AlbumCategoryViewModel> AlbumCategoryViewModel { get; set; }
+        public DbSet<ImageProject.ViewModels.MyMediaViewModels.MyMediaViewModel> MyMediaViewModel { get; set; }
+        public DbSet<ImageProject.ViewModels.AlbumCategoryViewModels.CreateAlbumCategoryViewModel> CreateAlbumCategoryViewModel { get; set; }
+        public DbSet<ImageProject.ViewModels.AlbumCategoryViewModels.EditAlbumCategoryViewModel> EditAlbumCategoryViewModel { get; set; }
     }
 }
